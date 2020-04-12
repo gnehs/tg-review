39731f967c901df3a87eaf9d69edbe8f2f071ec0
gnehs <jayuiop@gmail.com>
2020-04-12 16:40:38 +0800

    RWD

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,16 @@
 <template>
-	<div style="width:500px;margin: 10px auto;">
+	<div style="width:500px;max-width:calc(100vw - 20px);margin: 10px auto;">
 		<v-row v-show="data&&categories&&data.length>0">
-			<transition-group tag="div" name="songlist" style="position: relative">
+			<transition-group
+				tag="div"
+				name="songlist"
+				style="position: relative;min-width: 500px;max-width:calc(100vw - 20px);"
+			>
 				<template v-for="item of data">
-					<v-col cols="12" :key="item.id">
-						<v-card class="mx-auto" max-width="500" min-width="500">
+					<v-col cols="12" :key="item.id" width="100%">
+						<v-card class="mx-auto" width="100%">
 							<v-card-text>
-								<div class="overline mb-4">#{{item.id}}</div>
+								<div class="overline mb-4">#{{item.id}} @{{item.meta['tg-link'].split('/').slice(-1).pop()}}</div>
 								<p class="display-1 text--primary">{{item.title.rendered}}</p>
 
 								<v-chip
@@ -18,34 +22,40 @@
 								<br />
 								<div class="text--primary" v-html="item.content.rendered" />
 							</v-card-text>
+							<v-divider />
 							<v-card-actions>
+								<v-btn
+									text
+									:href="`https://tg.gnehs.net/wp-admin/post.php?action=edit&post=${item.id}`"
+									target="_blank"
+								>編輯</v-btn>
 								<v-tooltip top>
 									<template v-slot:activator="{ on }">
 										<v-btn text :href="item.meta['tg-link']" target="_blank" v-on="on">前往連結</v-btn>
 									</template>
 									<span>{{item.meta['tg-link']}}</span>
 								</v-tooltip>
-								<v-btn
-									text
-									:href="`https://tg.gnehs.net/wp-admin/post.php?action=edit&post=${item.id}`"
-									target="_blank"
-								>編輯</v-btn>
 
 								<v-spacer></v-spacer>
-								<v-btn text @click="updatePost('delete',item.id)">刪除</v-btn>
-								<v-btn text @click="updatePost('publish',item.id)" color="deep-purple accent-4">發布</v-btn>
+								<v-btn text color="error" @click="updatePost('delete',item.id)">刪除</v-btn>
+								<v-btn dark @click="updatePost('publish',item.id)" color="deep-purple accent-4">發布</v-btn>
 							</v-card-actions>
 						</v-card>
 					</v-col>
 				</template>
 			</transition-group>
 		</v-row>
-		<div v-show="!loading&&data&&data.length==0">
-			<v-card class="mx-auto" max-width="500">
+		<div v-show="!loading&&data&&data.length==0" width="100%">
+			<v-card class="mx-auto">
 				<v-card-text>
 					<p class="display-1 text--primary">空</p>
 					<div class="text--primary">尚無項目可供審核！</div>
 				</v-card-text>
+				<v-divider />
+				<v-card-actions>
+					<v-spacer></v-spacer>
+					<v-btn text href="https://tg.gnehs.net/wp-admin/" target="_blank">控制台</v-btn>
+				</v-card-actions>
 			</v-card>
 		</div>
 		<div class="text-center" v-show="loading||!data">
